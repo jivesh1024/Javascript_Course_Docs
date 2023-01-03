f44bb56deb2c0f12ad1edb906d6a0ad4e8b3758c
jivesh1024 <jiveshshrivastav@gmail.com>
2023-01-04 02:05:40 +0530

    added funtion in app.js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,4 +2,15 @@ const defaultResult = 0
 let currentData = defaultResult;
 currentData = currentData + 10;
 let calculationDescription = 'The result is';
-outputResult(currentData,calculationDescription)
+
+function add(num1, num2)
+{
+    const result = num1 + num2;
+    return result;
+}
+
+currentData = add(1,3);
+
+
+outputResult(currentData,calculationDescription);
+
